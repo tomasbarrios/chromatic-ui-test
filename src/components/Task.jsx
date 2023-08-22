1d2ca4fa3a16059d52ca21a88775ce4a15d81288
Tomas Barrios <tomasbarrios@gmail.com>
2023-08-22 14:17:33 -0400

    add chromatic

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 export default function Task({
   task: { id, title, state },
   onArchiveTask,
@@ -50,7 +50,7 @@ export default function Task({
           aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
           key={`pinTask-${id}`}
         >
-          <span className={`icon-bell`} />
+         <span className={`icon-star`} />
         </button>
       )}
     </div>
